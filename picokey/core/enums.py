872aca7c83744cf45e5a433aaefdd8581fbabfb8
Pol Henarejos <pol.henarejos@cttc.es>
2025-11-26 11:53:57 +0100

    Add format check for enums.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- picokey/core/enums.py
+++ picokey/core/enums.py
@@ -1,5 +1,25 @@
 import enum
+from typing import Union
 
 class NamedIntEnum(enum.IntEnum):
     def __str__(self):
         return self.name
+
+    def __format__(self, fmt):
+        if any(c in fmt for c in "xXod"):
+            return format(self.value, fmt)
+        return self.name
+
+    @classmethod
+    def from_string(cls, value: Union[str, int]) -> "NamedIntEnum":
+        if not value:
+            return cls.UNKNOWN
+
+        value = value.strip().lower()
+
+        for member in cls:
+            if member.value == value or member.name.lower() == value:
+                return member
+
+        return cls.UNKNOWN
+
